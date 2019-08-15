074d2877809a208c1b05de9e1bd5debd990d7f56
Aashish Karki <aashish.y2z@gmail.com>
2019-08-15 21:00:10 +0545

    add lib

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import TypeAhead from 'typeahead-react-skeleton';
+// import TypeAhead from 'typeahead-react-skeleton';
 import './App.css';
 
 
 function App() {
   return (
     <div style={{ margin: '100px', width: '600px' }}>
-      <TypeAhead
+      {/* <TypeAhead
         options={['Apple', 'Aeroplane', 'Ball', 'Basker']}
         onSelect={v => alert(`You selected ${v}`)}
-      />
+      /> */}
     </div>
   );
 }
