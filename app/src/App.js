ffef9f5d807191cc4446b38216247d8b7ff9dc98
Aashish Karki <aashish.y2z@gmail.com>
2019-08-15 20:55:27 +0545

    initate project

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import TypeAhead from 'typeahead-react-skeleton';
+import './App.css';
+
+
+function App() {
+  return (
+    <div style={{ margin: '100px', width: '600px' }}>
+      <TypeAhead
+        options={['Apple', 'Aeroplane', 'Ball', 'Basker']}
+        onSelect={v => alert(`You selected ${v}`)}
+      />
+    </div>
+  );
+}
+
+export default App;
