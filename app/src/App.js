5057e19bc28e90eed8e06622306175d979be9573
Aashish Karki <aashish.y2z@gmail.com>
2019-08-18 20:15:13 +0545

    app update

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import VirtualizedList from 'virtualized-list-skeleton';
 import './App.css';
 
 
 function App() {
-  return (
-    <div style={{ margin: '100px', width: '600px', background: 'red' }}>
-      <VirtualizedList
-        data={['Apple', 'Aeroplane', 'Ball', 'Basker']}
-        renderItem={(item) => {
-          console.log(item);
-          return (
-            <div>
+  const [data, setData] = useState(() => {
+    return Array(10).fill(null).map((d, idx) => idx);
+  });
 
+  const changeHandler = useCallback((value) => {
+    setData(() => {
+      return Array(parseInt(value, 10)).fill(null).map((d, idx) => idx);
+    });
+  }, []);
+
+  return (
+    <div style={{ margin: '100px', width: '600px', background: 'lightred', border: '1px solid' }}>
+      Number of data to render :
+      <input type="number" onChange={e => changeHandler(e.target.value)} placeholder="number of data to render" value={data.length} /> <br />
+      <VirtualizedList
+        data={data}
+        renderItem={(item) => {
+          return (
+            <div style={{ margin: '30px', background: 'lightgray' }}>
+              {item}
             </div>
           );
         }}
